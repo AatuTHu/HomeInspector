a9ef36f23faa0d3116f6bfed7647d708fbe7f23c
Aatu Huttula <91623487+AatuTHu@users.noreply.github.com>
2023-11-08 14:26:50 +0200

    Backend update
    Deleted firebase.js from gitIgnore

---
+++ backend/firebase.js
@@ -0,0 +1,33 @@
+const { initializeApp } = require("firebase/app");
+const { getFirestore, collection, onSnapshot, query, doc, getDocs,addDoc,orderBy } = require("firebase/firestore");
+const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } = require("firebase/auth");
+
+
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+const firestore = getFirestore();
+const HUMIDITY = "humidity"
+const TEMPERATURE = "temperature"
+
+module.exports = {
+  firestore,
+  HUMIDITY,
+  TEMPERATURE,
+  collection,
+  addDoc,
+  orderBy,
+  onSnapshot,
+  query,
+  doc,
+  getDocs
+}
