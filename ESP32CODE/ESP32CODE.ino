9277ac715cdb494c4103f4f9e61bf13e5d7b8e90
Aatu Huttula <91623487+AatuTHu@users.noreply.github.com>
2023-12-05 17:54:51 +0200

    ESP32
    Added 3 buttons to board so that the user can control the device without phone also.

--- ESP32CODE/ESP32CODE.ino
+++ ESP32CODE/ESP32CODE.ino
@@ -8,6 +8,9 @@
 
 #include "CREDENTIALS.h"
 #define ledPin 5
+#define buttonLedPin 14
+#define buttonTempPin 27
+#define buttonHumPin 26
 #define DHTPIN 0
 #define DHTTYPE DHT22
 DHT dht(DHTPIN,DHTTYPE);
@@ -15,207 +18,265 @@ DHT dht(DHTPIN,DHTTYPE);
 AsyncWebServer server(80);
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(23, 22, 17, 19, 18);
-int contrastValue = 200; // Default Contrast Value
+
 
 //Variables
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
+int contrastValue = 150;
+
 const char* temperature = "temperature";
 const char* humidity = "humidity";
 const char* start = "start";
 const char* tempUnit = "celsius";
 const char* humUnit = "%";
 
+int tempButtonState = LOW;
+int lastTempButtonState = LOW;
+int humButtonState = LOW;
+int lastHumButtonState = LOW;
+int buttonLedState = LOW;
+int lastButtonLedState = LOW;
 int enableMeasurementsTemperature = 0;
 int enableMeasurementsHumidity = 0; 
 int enableLights = 0;
 
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 unsigned long previousMillis = 0;
 unsigned long interval = 10000;
 
-void makeHttpPOSTRequestToServer(const char* URL,const char* KEY, float VALUE) {
-HTTPClient http;
-// Specify the server endpoint and request method
-http.begin(URL);
-http.addHeader("Content-Type", "application/json");
-
-// Your JSON payload
- String jsonPayload = "{\""+ String(KEY) +"\":" + String(VALUE) + ", \"apiKey\":\"" + String(apiKey) + "\"}";
-
-// Send the POST request
- int httpResponseCode = http.POST(jsonPayload);
-
-if (httpResponseCode > 0) {
- Serial.print("HTTP Response code: ");
- Serial.println(httpResponseCode);
- String response = http.getString();
- Serial.println(response);
-} else {
- Serial.print("Error on HTTP request. HTTP Response code: ");
- Serial.println(httpResponseCode);
-}
- http.end();
-}
-
-
 void setup() {
-Serial.begin(115200);
-pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
 
-WiFi.begin(ssid, password);
-while (WiFi.status() != WL_CONNECTED) {
-Serial.print("..");
-}
-Serial.println("Connected to WiFi");
-Serial.println(WiFi.localIP());
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonLedPin,INPUT);
+  pinMode(buttonTempPin, INPUT);
+  pinMode(buttonHumPin, INPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
 
 
-server.on("/ledON", HTTP_GET, [](AsyncWebServerRequest *request){
- enableLights = 1;
-request->send(200, "text/plain", "LED is ON");
+  server.on("/ledON", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableLights = 1;
+  request->send(200, "text/plain", "LED is ON");
+  });
 
-display.clearDisplay();
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.setCursor(0, 0);
-display.println("LED is ON");
-display.display();
-});
+  server.on("/ledOFF", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableLights = 0;
+  request->send(200, "text/plain", "LED is OFF");
+  });
 
-server.on("/ledOFF", HTTP_GET, [](AsyncWebServerRequest *request){
- enableLights = 0;
-request->send(200, "text/plain", "LED is OFF");
+  server.on("/tempON", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableMeasurementsTemperature = 1;
+  request->send(200, "text/plain", "Temperature is ON");
+  });
 
-display.clearDisplay();
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.setCursor(0, 0);
-display.println("LED is OFF");
-display.display();
-});
+  server.on("/tempOFF", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableMeasurementsTemperature = 0;
+  request->send(200, "text/plain", "Temperature is OFF");
+  });
 
-server.on("/tempON", HTTP_GET, [](AsyncWebServerRequest *request){
- enableMeasurementsTemperature = 1;
-request->send(200, "text/plain", "Temperature is ON");
-});
+  server.on("/humON", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableMeasurementsHumidity = 1;
+  request->send(200, "text/plain", "Humidity is ON");
+  });
 
-server.on("/tempOFF", HTTP_GET, [](AsyncWebServerRequest *request){
- enableMeasurementsTemperature = 0;
-request->send(200, "text/plain", "Temperature is OFF");
-});
+  server.on("/humOFF", HTTP_GET, [](AsyncWebServerRequest *request){
+  enableMeasurementsHumidity = 0;
+  request->send(200, "text/plain", "Humidity is OFF");
+  });
 
-server.on("/humON", HTTP_GET, [](AsyncWebServerRequest *request){
- enableMeasurementsHumidity = 1;
-request->send(200, "text/plain", "Humidity is ON");
-});
+  server.begin();
+  dht.begin();
+  display.begin();
 
-server.on("/humOFF", HTTP_GET, [](AsyncWebServerRequest *request){
- enableMeasurementsHumidity = 0;
-request->send(200, "text/plain", "Humidity is OFF");
-});
-
-server.begin();
-dht.begin();
-display.begin();
-
-display.setContrast(contrastValue);
-display.clearDisplay();
-display.display();
-
-display.setTextColor(BLACK, WHITE);
-display.setCursor(0,0);
-display.setTextSize(0.5);
-display.println("HOMEINSPECTOR ONLINE");
-display.display();
+  display.setContrast(contrastValue);
+  display.clearDisplay();
+  display.display();
 }
 
-void bothMeasurementsON() {
-  
-display.clearDisplay();
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.setCursor(0,1);
+void makeHttpPOSTRequestToServer(const char* URL,const char* KEY, float VALUE) {
+  HTTPClient http;
+  // Specify the server endpoint and request method
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/json");
+  // Your JSON payload
+  String jsonPayload = "{\""+ String(KEY) +"\":" + String(VALUE) + ", \"apiKey\":\"" + String(apiKey) + "\"}";
+  // Send the POST request
+  int httpResponseCode = http.POST(jsonPayload);
 
-display.println("Temperature");
-display.print(temp);
-display.println(" Celsius");
-display.println("");
-
-display.println("Humidity");
-display.print(hum);
-display.println(" %");
-if(enableLights == 1) {
-  display.println("LED is ON");
+  if (httpResponseCode > 0) {
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  String response = http.getString();
+  Serial.println(response);
+  } else {
+  Serial.print("Error on HTTP request. HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  }
+  http.end();
 }
-display.display();
+
+void startScreen() {
+  display.clearDisplay();
+  display.setTextColor(BLACK, WHITE);
+  display.setCursor(0,0);
+  display.setTextSize(0.5);
+  display.println("HOMEINSPECTOR ONLINE");
+  display.display();
+}
+
+
+void bothMeasurementsON() {    
+  display.clearDisplay();
+  display.setTextColor(BLACK, WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,1);
+
+  display.println("Temperature");
+  display.print(temp);
+  display.println(" Celsius");
+  display.println("");
+
+  display.println("Humidity");
+  display.print(hum);
+  display.println(" %");
+  if(enableLights == 1) {
+  display.println("LED IS ON");
+  } else {
+  display.println("LED IS OFF");
+  }
+  display.display();
 }
 
 void onlyOneMeasurementON(const char* KEY, float VALUE, const char* UNIT) {
-display.clearDisplay();
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.setCursor(0, 1);
-display.println(KEY);
-display.print(VALUE);
-display.print(" ");
-display.println(UNIT);
-if(enableLights == 1) {
-  display.println("LED is ON");
-}
-display.display();
+  display.clearDisplay();
+  display.setTextColor(BLACK, WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,1);
+  display.println(KEY);
+  display.print(VALUE);
+  display.print(" ");
+  display.println(UNIT);
+  if(enableLights == 1) {
+  display.println("LED IS ON");
+  } else {
+  display.println("LED IS OFF");
+  }
+  display.display();
 }
 
 void noMeasurementsON() {
-display.clearDisplay();
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.setCursor(1,1);
-display.println("Temperature");
-display.println("and");
-display.println("humidity");
-display.println("are off");
-display.println("");
-if(enableLights == 1) {
-  display.println("LED is ON");
-}
-display.display();
+  display.clearDisplay();
+  display.setTextColor(BLACK, WHITE);
+  display.setTextSize(1);
+  display.setCursor(1,1);
+  display.println("Temperature");
+  display.println("and");
+  display.println("humidity");
+  display.println("are off");
+  display.println("");
+  if(enableLights == 1) {
+  display.println("LED IS ON");
+  } else {
+  display.println("LED IS OFF");
+  }
+  display.display();
 }
 
 void loop() {
- unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
+  int ledReading = digitalRead(buttonLedPin);
+  int tempReading = digitalRead(buttonTempPin);
+  int humReading = digitalRead(buttonHumPin);
 
-if(currentMillis - previousMillis > interval) {
 
-if(enableMeasurementsTemperature == 1) { // user can enable measurements from phone.
-temp= dht.readTemperature();
-Serial.println("Temperature");
-Serial.print(temp);
-Serial.println(" Celsius");
-makeHttpPOSTRequestToServer(temperatureURL, temperature, temp);
+  
+  //LED BUTTON
+
+  if (ledReading != lastButtonLedState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+   if (ledReading != buttonLedState) {
+      buttonLedState = ledReading;
+      if (buttonLedState == HIGH) {
+        enableLights = 1 - enableLights;
+      }
+    }
+  }
+
+  lastButtonLedState = ledReading;
+
+  //TEMPERATURE BUTTON
+
+  if (tempReading != lastTempButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (tempReading != tempButtonState) {
+      tempButtonState = tempReading;
+      if (tempButtonState == HIGH) {
+        enableMeasurementsTemperature = 1 - enableMeasurementsTemperature;
+      }
+    }
+  }
+
+  lastTempButtonState = tempReading;
+
+  //HUMIDITY BUTTON
+
+  if (humReading != lastHumButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (humReading != humButtonState) {
+      humButtonState = humReading;
+      if (humButtonState == HIGH) {
+        enableMeasurementsHumidity = 1 - enableMeasurementsHumidity;
+      }
+    }
+  }
+
+  lastHumButtonState = humReading;
+
+  //READING POSTS
+
+  if(currentMillis - previousMillis > interval) {
+    if(enableMeasurementsTemperature == 1) { // user can enable measurements from phone.
+    temp = dht.readTemperature();
+    makeHttpPOSTRequestToServer(temperatureURL, temperature, temp);
+    }
+    if(enableMeasurementsHumidity == 1) { // user can enable measurements from phone.
+    hum = dht.readHumidity();
+    makeHttpPOSTRequestToServer(humidityURL, humidity, hum);
+    }
+    previousMillis = currentMillis;
+  }
+
+  if(enableMeasurementsHumidity == 1 && enableMeasurementsTemperature == 1) {
+    bothMeasurementsON();
+  } else if (enableMeasurementsHumidity == 1 ) {
+    onlyOneMeasurementON(humidity, hum, humUnit);
+  } else if(enableMeasurementsTemperature == 1) {
+    onlyOneMeasurementON(temperature, temp, tempUnit);
+  } else if(enableMeasurementsTemperature == 0 && enableMeasurementsHumidity == 0) {
+    noMeasurementsON();
+  }
+
+  if(enableLights == 1) {
+    digitalWrite(ledPin, HIGH);
+  } else if (enableLights == 0) {
+    digitalWrite(ledPin, LOW);
+ }
 }
-
-if(enableMeasurementsHumidity == 1) { // user can enable measurements from phone.
- hum = dht.readHumidity();
- Serial.println("Humidity: ");
- Serial.print(hum);
- Serial.println(" %");
- makeHttpPOSTRequestToServer(humidityURL, humidity, hum);
-}
- previousMillis = currentMillis;
-}
-
-if(enableMeasurementsHumidity == 1 && enableMeasurementsTemperature == 1) {
-bothMeasurementsON();
-} else if (enableMeasurementsHumidity == 1 ) {
-onlyOneMeasurementON(humidity, hum, humUnit);
-} else if(enableMeasurementsTemperature == 1) {
-onlyOneMeasurementON(temperature, temp, tempUnit);
-} else if(enableMeasurementsTemperature == 0 && enableMeasurementsHumidity == 0) {
-noMeasurementsON();
-}
-
-if(enableLights == 1) {
-digitalWrite(ledPin, HIGH);
-} else if (enableLights == 0) {
-digitalWrite(ledPin, LOW);
-}}
